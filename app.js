3cd9bf4bae2ef77ec67c53259efa76f73e1da765
KhersonV <khersonv110990@gmail.com>
2023-01-18 07:29:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -85,6 +85,21 @@ function minus(e){
     checkValue();
  }
 
+//  //weather
+// const API = "216fba67b97500eb7322c4b92f1812e5"
+//  fetch(`http://api.openweathermap.org/data/2.5/weather?id=${}&lang=ru&appid=${API}`).then(function (resp) {return resp.json() }).then(function (data) {
+//     //добавляем название города
+//     document.querySelector('.weather__city').textContent = data.name;
+//     //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
+//     document.querySelector('.weather__forecast').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+//     //Добавляем описание погоды
+//     document.querySelector('.weather__desc').textContent = data.weather[0]['description'];
+//     //Добавляем иконку погоды
+//     document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+//     })
+//     .catch(function () {
+//         //Обрабатываем ошибки
+//     });
 
 // class Counter {
 //     constructor(incrementBtn, decrementBtn, inputField) {
